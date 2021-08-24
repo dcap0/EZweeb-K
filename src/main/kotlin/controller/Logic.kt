21e95187030caf303a62f4ae49f855e6c3b6cdd8
Dennis Capone <dennis.d.cap@gmail.com>
2021-08-23 22:06:13 -0600

    Added functions to parse nyaa html and download file.

--- src/main/kotlin/controller/Logic.kt
+++ src/main/kotlin/controller/Logic.kt
@@ -1,6 +1,19 @@
 package controller
 
 
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,11 +24,14 @@ import org.jsoup.Jsoup
 import org.jsoup.helper.HttpConnection
 import org.jsoup.nodes.Document
 import java.io.File
+import java.io.FileOutputStream
 import java.io.FileWriter
 import java.net.HttpURLConnection
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.nio.file.Path
 
 class Logic{
 
@@ -36,20 +52,61 @@ class Logic{
             retVal.add(
                 Series(
                     titles[i].text(),
-                    descriptions[i].text(),
-                    arrayListOf("google.com")
+                    descriptions[i].text()
                 )
             )
         }
         return retVal
     }
 
-    private fun getMagnetLinks(title: String){
-        val retVal = ArrayList<URI>()
-        val queryTitle = title.replace(" ", "+") + "+eng"
+    fun getDownloadLinks(title: String):HashMap<String,URI>{
+        val retVal = HashMap<String,URI>()
+        val queryTitle = title.replace(" ", "+") + "+sub"
         val doc: Document = Jsoup.connect("https://nyaa.si/?f=0&c=0_0&q=$queryTitle").get()
+        val downloadElements = doc.getElementsByClass("success")
+
+        downloadElements.forEach { x->
+            val key = if(x.child(1).children().size == 2){
+                x.child(1).child(1).text()
+            } else {
+                x.child(1).child(0).text()
+            }
+
+            val value = "https://nyaa.si/${x.child(2).child(0).attr("href")}"
+
+            retVal[key] = URI(value)
+        }
+        return retVal
     }
 
+    fun downloadShow(uri: URI, showName: String, fileName: String){
+        val path = System.getProperty("user.home") + File.separator  + "Downloads" + File.separator + "ezweebk" + File.separator + showName + File.separator
+        generateDownloadFolder(path)
+
+        val targetFile = File(path+fileName)
+
+        if(!targetFile.exists()) targetFile.createNewFile()
+
+        val stream = uri.toURL().openStream()
+        val fos = FileOutputStream(targetFile)
+
+        fos.write(stream.readBytes())
+
+        fos.flush()
+        stream.close()
+        fos.close()
+
+        Window (
+            size = IntSize(1200,100)
+        ){
+            Text(
+                text = "File has been saved: ${targetFile.absolutePath}",
+                modifier = Modifier.background(Color.LightGray).padding(4.dp).border(2.dp,Color.Cyan),
+                fontFamily = FontFamily.Monospace,
+                fontSize = 20.sp,
+            )
+        }
+    }
 
     //TODO figure out images.
 //    private fun getSeriesImageLinks(): ArrayList<String>{
@@ -61,15 +118,6 @@ class Logic{
 //        }
 //        return retVal
 //    }
-
-    private fun getSeriesData(htmlClass: String): ArrayList<String>{
-        val retVal = ArrayList<String>()
-        val doc: Document = Jsoup.connect("https://myanimelist.net/anime/season").get()
-        val titles = doc.getElementsByClass(htmlClass)
-        titles.forEach { retVal.add(it.text()) }
-        return retVal
-    }
-
 
     private fun initializeData(): String {
         val retData = getPath()
@@ -98,6 +146,14 @@ class Logic{
             writeDefault(dataFile)
         }
     }
+
+    private fun generateDownloadFolder(path: String){
+        val downloadDir = File(path)
+        if(!downloadDir.exists()){
+            downloadDir.mkdirs()
+        }
+    }
+
 
     private fun writeDefault(dataFile: File) {
         val fw = FileWriter(dataFile)
